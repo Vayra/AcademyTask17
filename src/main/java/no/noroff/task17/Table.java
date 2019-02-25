e418efdfb089ae6c26f903e5d1ca5c7c8d409759
Thomas G <thomas_grefsrud@hotmail.com>
2019-02-25 10:16:51 +0100

    Updates to table functions, made generic

--- src/main/java/no/noroff/task17/Table.java
+++ src/main/java/no/noroff/task17/Table.java
@@ -1,28 +1,65 @@
 package no.noroff.task17;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 /* Table class
  * Must be able to read update and delete entries
  *
  *
  */
-public class Table {
-    private Hashtable<Integer, String> table;
+public class Table<K, V>  {
+    private Hashtable<K, V> table;
+    private String tableName;
 
     public Table(){
         table = new Hashtable<>();
+        tableName = "Unnamed Table";
     }
 
-    public boolean contains(String query)
-    {
-        return table.containsValue(query);
-    }
-    public boolean contains(int query){
-        return table.containsKey(query);
+    public Table(String tableName){
+        table = new Hashtable<>();
+        this.tableName = tableName;
     }
 
-    public String get(int key){
+    public boolean contains(Object query){
+        return table.containsKey(query) || table.containsValue(query);
+    }
+
+    public V get(K key){
         return table.get(key);
+    }
+
+    public void read(){
+        // Prints key/value pairs to console
+        System.out.println(tableName + ": ");
+        for (Map.Entry<K, V> entry : table.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public boolean add(K key, V value){
+
+        if (!table.containsKey((key))){
+            table.put(key, value);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(K key, V newValue){
+        if (table.containsKey(key)){
+            table.replace(key, newValue);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remove(K key){
+        if (table.containsKey(key)){
+            table.remove(key);
+            return true;
+        }
+        return false;
     }
 }
