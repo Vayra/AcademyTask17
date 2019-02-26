d7e572c6087509c5da3e12ea620008560f6fa75e
Thomas G <thomas_grefsrud@hotmail.com>
2019-02-26 10:38:06 +0100

    Open/close connection to DB.

--- src/main/java/no/noroff/task17/Task17Application.java
+++ src/main/java/no/noroff/task17/Task17Application.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 @SpringBootApplication
 public class Task17Application {
-	public static ArrayList<contact> contacts;
+	public static ArrayList<contact> contacts = new ArrayList<>();
+	//public static ArrayList<family> families = new ArrayList<>();
 	private static Connection conn = null;
 	private static String URL = "jdbc:sqlite::resource:ContactInformationDB.db";
 
@@ -18,7 +21,24 @@ public class Task17Application {
 		SpringApplication.run(Task17Application.class, args);
 	}
 
-	public static void openConn(){}
+	public static void openConn(){
+		try {
+			conn = DriverManager.getConnection(URL);
+		}
+		catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void closeConn(){
+		try{
+			if (conn != null) conn.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
 
 	public static void readContact(){}
 
