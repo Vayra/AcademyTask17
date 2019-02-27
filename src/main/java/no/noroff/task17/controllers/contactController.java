0e2f171825061ea4d5e3da0122878fbcacabd5e8
Thomas G <thomas_grefsrud@hotmail.com>
2019-02-27 12:13:40 +0100

    added handling for /contact

--- src/main/java/no/noroff/task17/controllers/contactController.java
+++ src/main/java/no/noroff/task17/controllers/contactController.java
@@ -6,47 +6,65 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @RestController
 public class contactController {
 
-    @GetMapping("/contact/{ID}")
-    public contact contactFind(@PathVariable String ID){
-        System.out.println("Trying to find contact with ID= " + ID);
-        contact retContact = null;
+    @GetMapping("/contact")
+    public ArrayList<contact> contactFindDefault(){
+        System.out.println("Returning default search of ALL");
+        ArrayList<contact> retContacts = new ArrayList<>();
         Map<String, String> phone;
+
+        for (contact con : Task17Application.contacts){
+            if (!retContacts.contains(con)) retContacts.add(con);
+        }
+
+        return retContacts;
+    }
+
+    @GetMapping("/contact/{ID}")
+    public ArrayList<contact> contactFind(@PathVariable String ID){
+        System.out.println("Trying to find contact with ID= " + ID);
+        ArrayList<contact> retContacts = new ArrayList<>();
+        Map<String, String> phone;
+        System.out.println("ID: " + ID);
+        if (ID.equals(" ")) return  retContacts; // avoid error if empty ID
 
         //Search the contact list for the ID
         for (contact con : Task17Application.contacts){
             if (    con.getFirstName().contains(ID) ||
                     con.getLastName().contains(ID)    ){
-                System.out.println("----- Found contact");
-                retContact = con;
+                System.out.println("----- Found contact by Name");
+                if (!retContacts.contains(con)) retContacts.add(con);
             }
+
 
             phone = con.getPhone();
 
             for (String num :phone.values()){
                 if (num != null) {
                     if (num.contains(ID)) {
-                        System.out.println("----- Found contact");
-                        retContact = con;
+                        System.out.println("----- Found contact by Phone");
+                        if (!retContacts.contains(con)) retContacts.add(con);
                     }
                 }
             }
 
+            /*
             if (con.getContactID().equals(ID)){
-                System.out.println("----- Found contact");
-                retContact = con;
-            }
+                System.out.println("----- Found contact by ID");
+                if (!retContacts.contains(con)) retContacts.add(con);
+            }*/
 
         }
 
-        if (retContact == null){
+        if (retContacts == null){
             System.out.println("----- No contact found!");
         }
 
-        return retContact;
+        return retContacts;
     }
 }
