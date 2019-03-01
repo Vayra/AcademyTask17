9281851311806df7cfe33c6ec508ae36f6b4deeb
HeleneHarmens <helene.harmens@gmail.com>
2019-03-01 12:42:23 +0100

    added @response familySearch in familyControllers

--- src/main/java/no/noroff/task17/controllers/familyController.java
+++ src/main/java/no/noroff/task17/controllers/familyController.java
@@ -12,10 +12,10 @@ import static no.noroff.task17.Task17Application.*;
 @RestController
 public class familyController {
 
-    @GetMapping("/family/search/{ID}")
-    public ArrayList<family> findFamilyByName(@PathVariable String ID){
+    @RequestMapping("/familySearch/")
+    public ArrayList<family> findFamilyByName(@RequestParam(value="search", defaultValue ="Stian") String ID){
         ArrayList<contact> cons = new ArrayList<>();
-
+        System.out.println("ID: " + ID);
         for (contact c:contacts){
             if ((c.getFirstName() + " " + c.getLastName()).toLowerCase().contains(ID.toLowerCase())){
                 if(!cons.contains(c)) cons.add(c);
@@ -23,8 +23,11 @@ public class familyController {
             Map<String, String> phone = c.getPhone();
             for (String num : phone.values()){
 
-                if (num != null && num.contains(ID)){
-                    if (!cons.contains(c)) cons.add(c);
+                if (num != null){
+                    if (num.contains(ID)){
+                        System.out.println("I should not be here");
+                        if (!cons.contains(c)) cons.add(c);
+                    }
                 }
             }
         }
@@ -37,8 +40,9 @@ public class familyController {
         for (family fam : families)
         {
             String cID = fam.getContactID().split("/")[fam.getContactID().split("/").length - 1];
-            System.out.println(cID);
+            //System.out.println(cID);
             for (contact con: cons) {
+                //System.out.println(con.getFirstName());
                 if (cID.equals(con.getContactID())) {
                     System.out.println("----- FAMILY FOUND ---- ");
                     if (!returnFamilies.contains(fam)) returnFamilies.add(fam);
@@ -103,10 +107,156 @@ public class familyController {
                 if (!returnFamilies.contains(fam)) returnFamilies.add(fam);
             }
         }
+        if(returnFamilies.isEmpty()){
+            System.out.println("NOT FOUND");
+
+        }
+        return returnFamilies;
+    }
+
+    /*
+    @RequestMapping("/contactSearch")
+    public contact contactFinds(@RequestParam(value="search", defaultValue = "1") String ID ) {
+        System.out.println("Trying to find contact: " + ID);
+        contact returnContact = null;
+        Map<String, String> phone;
+        for (contact cust : Task17Application.contacts)
+        {
+            phone=cust.getPhone();
+            if ((cust.getFirstName() + " " + cust.getLastName()).contains(ID)
+            )
+
+            {
+
+                System.out.println(" --- CONTACT FOUND --- ");
+                returnContact = cust;
+            }
+            for (String num :phone.values()){
+                if (num != null) {
+                    if (num.contains(ID)) {
+                        System.out.println("----- Found contact by Phone");
+                        returnContact = cust;
+                    }
+                }
+            }
+        }
+        if(returnContact == null)
+        {
+            System.out.println(" --- CONTACT WAS NOT FOUND --- ");
+        }
+        return returnContact;
+    }
+    */
+
+    /*
+    @GetMapping("/contactSearch/{ID}")
+    public contact contactGet(@PathVariable String ID)
+    {
+        System.out.println("Trying to find contact: " + ID);
+        contact returnContact = null;
+        Map<String, String> phone;
+        for (contact cust : Task17Application.contacts)
+        {
+            phone=cust.getPhone();
+            if ((cust.getFirstName() + " " + cust.getLastName()).contains(ID)
+            )
+
+            {
+
+                System.out.println(" --- CONTACT FOUND --- ");
+                returnContact = cust;
+            }
+            for (String num :phone.values()){
+                if (num != null) {
+                    if (num.contains(ID)) {
+                        System.out.println("----- Found contact by Phone");
+                        returnContact = cust;
+                    }
+                }
+            }
+        }
+        if(returnContact == null)
+        {
+            System.out.println(" --- CONTACT WAS NOT FOUND --- ");
+        }
+        return returnContact;
+    }
+    */
+
+    /*
+    @RequestMapping("/familySearch")
+    public contact familyFinds(@RequestParam(value="search", defaultValue = "1") String ID ) {
+        System.out.println("Trying to find family: " + ID);
+        contact returnFamily = null;
+        Map<String, String> phone;
+
+        for (contact cust : contacts)
+        {
+            phone=cust.getPhone();
+            if ((cust.getFirstName() + " " + cust.getLastName()).contains(ID)
+            )
+
+            {
+
+                System.out.println(" --- CONTACT FOUND --- ");
+                returnFamily = cust;
+            }
+            for (String num :phone.values()){
+                if (num != null) {
+                    if (num.contains(ID)) {
+                        System.out.println("----- Found contact by Phone");
+                        returnFamily = cust;
+                    }
+                }
+            }
+        }
+        if(returnFamily == null)
+        {
+            System.out.println(" --- CONTACT WAS NOT FOUND --- ");
+        }
+        return returnFamily;
+    }
+
+
+    @GetMapping("/familySearch/{ID}")
+    public ArrayList<family> familyGet (@PathVariable String ID) {
+        ArrayList<contact> cons = new ArrayList<>();
+
+        for (contact c:contacts){
+            if ((c.getFirstName() + " " + c.getLastName()).toLowerCase().contains(ID.toLowerCase())){
+                if(!cons.contains(c)) cons.add(c);
+            }
+            Map<String, String> phone = c.getPhone();
+            for (String num : phone.values()){
+
+                if (num != null && num.contains(ID)){
+                    if (!cons.contains(c)) cons.add(c);
+                }
+            }
+        }
+
+        System.out.println("Trying to find relative: " + ID);
+        ArrayList<family> returnFamilies = new ArrayList<>();
+
+        if (cons.size() == 0) return returnFamilies;
+
+        for (family fam : families)
+        {
+            String cID = fam.getContactID().split("/")[fam.getContactID().split("/").length - 1];
+            System.out.println(cID);
+            for (contact con: cons) {
+                if (cID.equals(con.getContactID())) {
+                    System.out.println("----- FAMILY FOUND ---- ");
+                    if (!returnFamilies.contains(fam)) returnFamilies.add(fam);
+                }
+            }
+        }
         if(returnFamilies == null){
             System.out.println("NOT FOUND");
 
         }
         return returnFamilies;
     }
+    */
+
 }
